790e2b17218afa6aa585a439e9bd5a6c9baa6c44
Nadav20 <Levy.nadav@campus.technion.ac.il>
2023-04-03 12:56:41 +0300

    adding q1 to repository

---
+++ q1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int main(){
+//    printf("hello world");
+//    return 0;
+/*
+    printf("Enter size of input");
+    int num = scanf("%f", &num);
+    if(num <= 0){
+        printf("Invalid size");
+        return 0;
+    }
+    printf("Enter numbers");
+    int numbers[num] = {0};
+    for(int i=0; i<num; i++){
+        numbers[i] = scanf("%f", &d); 
+    }       
+        int j=0;
+    while()
+    }
+    */
+    printf("aaaa");
+
+}
